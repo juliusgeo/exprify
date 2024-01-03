22b052f43a8ae348a262c4bd61da7aa497b3b337
Julius Park <juliusgeo@gmail.com>
2024-01-02 21:33:22 -0500

    fix final issues with reflowing

--- exprify/reflow.py
+++ exprify/reflow.py
@@ -14,9 +14,29 @@ TOLERANCE = 4
 Token = namedtuple("Token", ["string", "type"])
 
 
+def partition_token(tok, space):
+    ts = tok.string
+    splpt = ts.find(".") if tok.type == NAME else space
+    # Checks to make sure we aren't in the middle of an f-string
+    if "f'" in ts:
+        for i in range(space - TOLERANCE, space + TOLERANCE + 1):
+            if ts[:i].count("{") == ts[:i].count("}"):
+                splpt = i
+    septok = "" if tok.type == NAME else "'"
+    return Token(string=ts[:splpt] + septok, type=tok.type), Token(
+        string=septok + ts[splpt:], type=tok.type
+    )
+
+
+def generate_whitespace_groups(line):
+    return [
+        (is_space, len(list(group)))
+        for is_space, group in groupby(line, key=str.isspace)
+    ]
+
+
 def reflow(script, outline):
-    # Transpile and THEN minify the script (doesn't work the other way for some reason).
-    # Can't rename locals because python_minifier doesn't like the weird scoping of using lambdas everywhere.
+    # Minify script and then transpile it
     with open(script, "r") as f:
         script = f.read()
         script = python_minifier.minify(
@@ -32,28 +52,7 @@ def reflow(script, outline):
         Token(tok.string.strip(), tok.type)
         for tok in tokenize(io.BytesIO(bytes(script, "utf-8")).readline)
     ]
-
-    def partition_token(tok, space):
-        ts = "".join(tok.string.split(" "))
-        splpt = ts.find(".") if tok.type == NAME else space
-        septok = "" if tok.type == NAME else "'"
-        return Token(string=ts[:splpt] + septok, type=tok.type), Token(
-            string=septok + ts[splpt:], type=tok.type
-        )
-
-    new_lines = ["(\n);"]
-
-    def generate_whitespace_groups(line):
-        # If there is a whitespace group that is a small size, then merge it into the previous non whitespace group
-        groups = []
-        for is_space, group in groupby(line, key=str.isspace):
-            group_size = len(list(group))
-            # if groups and is_space and group_size <= 2:
-            #     prev_sp, prev_size = groups.pop(-1)
-            #     groups.append((prev_sp, group_size + prev_size))
-            # else:
-            groups.append((is_space, group_size))
-        return groups
+    new_lines = "(\n);"
 
     for line in open(outline).readlines():
         line = line.rstrip()
@@ -68,11 +67,7 @@ def reflow(script, outline):
                     if not token_list:
                         break
                     if len(token_list[0].string.strip()) > space + TOLERANCE:
-                        # In Python 3.12, should be able to detect fstrings better
-                        if (
-                            token_list[0].type in (STRING, NAME)
-                            and "f'" not in token_list[0].string
-                        ):
+                        if token_list[0].type in (STRING, NAME):
                             # We can't split NAMEs if they don't have a dot in them
                             if (
                                 token_list[0].type == NAME
@@ -83,6 +78,13 @@ def reflow(script, outline):
                             left, right = partition_token(token_list.pop(0), space)
                             token_list.insert(0, right)
                             cur_token = left
+                        elif (
+                            old.type == STRING
+                            or token_list[0].type == STRING
+                            and space > 0
+                        ):
+                            # If the previous token was a string, we can just append "" to the end of it, which will be ignored
+                            cur_token = Token(string="", type=STRING)
                         else:
                             # We can't resize, so continue to the next group :(
                             break
@@ -97,14 +99,14 @@ def reflow(script, outline):
                         case _:
                             # Need to add a space between NAMES and (keywords or NAMES)
                             if (
-                                (
-                                    (iskeyword(tok_str) or cur_token.type == NAME)
-                                    and old.type == NAME
-                                )
-                                and cur_line
-                                and not cur_line[-1].isspace()
-                            ):
-                                tok_str = " " + tok_str
+                                iskeyword(tok_str) or cur_token.type == NAME
+                            ) and old.type == NAME:
+                                if cur_line and cur_line[-1] != " ":
+                                    tok_str = " " + tok_str
+                                elif (
+                                    not cur_line and new_lines and new_lines[-1] != " "
+                                ):
+                                    tok_str = " " + tok_str
                     old = cur_token
                     space -= len(tok_str)
                     cur_line += tok_str
@@ -112,5 +114,5 @@ def reflow(script, outline):
         if len(token_list) > 0:
             cur_line = cur_line + "\\\n"
         if cur_line:
-            new_lines.append(cur_line)
+            new_lines += cur_line
     return "".join(new_lines)
